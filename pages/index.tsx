63c2ed7e0da340d7d91dbe647fc6575c45c8aff4
= <=>
2020-11-17 15:20:13 -0500

    [ADD] fetch data connected to home and product page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../components/Navbar/Navbar'
 
 const HomePage = () => {
+  const [productList, setProductLista] = useState<TProduct[]>([])
+  useEffect(() => {
+    fetch('/api/avo')
+      .then((response) => response.json())
+      .then(({ data }) => setProductLista(data))
+  }, [])
   return (
     <div>
       <Navbar />
       <div>Platzi and Next.js!</div>
+      {productList.map((product) => (
+        <div>{product.image}</div>
+      ))}
     </div>
   )
 }
--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 
 const ProductPage = () => {
-  const { query } = useRouter()
+  const [product, setProduct] = useState<TProduct>()
+  const {
+    query: { id },
+  } = useRouter()
+
+  useEffect(() => {
+    if (id) {
+      window
+        .fetch(`/api/avo/${id}`)
+        .then((response) => response.json())
+        .then((avo) => setProduct(avo))
+      console.log(product)
+    }
+  }, [id])
 
   return (
     <section>
-      <h1>Página producto: {query.id}</h1>
+      <h1>Página producto: {product?.name}</h1>
     </section>
   )
 }
