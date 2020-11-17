491b3e9e4f1e930623bb33519ef5542565dd7604
= <=>
2020-11-17 16:01:08 -0500

    [ADD] styles added as example

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -3,10 +3,17 @@ import Navbar from '@components/Navbar/Navbar'
 
 const Layout: React.FC = ({ children }) => {
   return (
-    <div>
+    <div className="container">
       <Navbar />
       {children}
       <footer>This is the footer</footer>
+      <style jsx>
+        {`
+          .container {
+            background-color: salmon;
+          }
+        `}
+      </style>
     </div>
   )
 }
